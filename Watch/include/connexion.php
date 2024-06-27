19f3e00983c29df1d7b7edc563294232f10f92f9
Fifaliana RABENARISON <tsiferantsoaf@gmail.com>
2024-06-27 13:24:58 +0100

    final portfolio

--- Watch/include/connexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
-    $db = "watch_bdd";
-    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-?>
